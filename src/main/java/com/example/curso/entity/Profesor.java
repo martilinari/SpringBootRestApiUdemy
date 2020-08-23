052278499333ac9ff603c8cecf69654f62bbbae2
martilinari <martinianolinari@gmail.com>
2020-08-23 18:36:16 +0200

    added join to middle languaje table

--- src/main/java/com/example/curso/entity/Profesor.java
+++ src/main/java/com/example/curso/entity/Profesor.java
@@ -3,7 +3,9 @@ package com.example.curso.entity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,11 +14,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 @Table(name = "profesores")
@@ -28,7 +34,7 @@ public class Profesor implements Serializable {
 	private Long id;
 
 	@Column(name = "nombre")
-	private String name; 
+	private String name;
 
 	@Column(length = 60, unique = true)
 	private String email;
@@ -41,11 +47,16 @@ public class Profesor implements Serializable {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
-	
+
 	@OneToMany(cascade = CascadeType.ALL)
 	@JoinColumn(name = "profesor_id", referencedColumnName = "id")
 	private List<Curso> listCursos = new ArrayList<>();
-	
+
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JsonBackReference
+	@JoinTable(name = "profesores_lenguajes", joinColumns = @JoinColumn(name = "profesor_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "lenguaje_id", referencedColumnName = "id"))
+	private Set<Lenguaje> lenguajes = new HashSet<Lenguaje>();
+
 	@PrePersist
 	public void prePersist() {
 		createAt = new Date();
@@ -77,6 +88,14 @@ public class Profesor implements Serializable {
 
 	public String getEmail() {
 		return email;
+	}
+
+	public Set<Lenguaje> getLenguajes() {
+		return lenguajes;
+	}
+
+	public void setLenguajes(Set<Lenguaje> lenguajes) {
+		this.lenguajes = lenguajes;
 	}
 
 	public void setEmail(String email) {
