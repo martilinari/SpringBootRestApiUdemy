baffd8fa3751485c97778a3fd4e578cfad480531
martiniano linari <martinianolinari@gmail.com>
2020-08-23 17:08:15 +0200

    nueva entidad tabla many to many

---
+++ src/main/java/com/example/curso/entity/Lenguaje.java
@@ -0,0 +1,72 @@
+package com.example.curso.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "lenguaje")
+public class Lenguaje implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nombre")
+	private String name;
+
+	@Column(name = "date")
+	@JsonFormat(pattern = "YYY-MM-dd")
+	private Date date;
+
+	@ManyToMany
+	@JoinTable(name = "profesores_lenguajes", joinColumns = @JoinColumn(name = "lenguaje_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "profesor_id", referencedColumnName = "id"))
+	private Set<Profesor> profesores = new HashSet<Profesor>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Set<Profesor> getProfesores() {
+		return profesores;
+	}
+
+	public void setProfesores(Set<Profesor> profesores) {
+		this.profesores = profesores;
+	}
+
+}
