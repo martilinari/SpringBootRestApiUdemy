b4d3232ed780316721dd4491fa0f38a147e56d9c
martilinari <martinianolinari@gmail.com>
2020-08-23 17:24:52 +0200

    added date

--- src/main/java/com/example/curso/entity/Lenguaje.java
+++ src/main/java/com/example/curso/entity/Lenguaje.java
@@ -29,6 +29,10 @@ public class Lenguaje implements Serializable {
 	@Column(name = "nombre")
 	private String name;
 
+	@Column(name = "date")
+	@JsonFormat(pattern = "YYY-MM-dd")
+	private Date date;
+
 	@ManyToMany
 	@JoinTable(name = "profesores_lenguajes", joinColumns = @JoinColumn(name = "lenguaje_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "profesor_id", referencedColumnName = "id"))
 	private Set<Profesor> profesores = new HashSet<Profesor>();
